8b55918db1772c6d2774b6f5c338130491147654
Elfi Yang <elfiyang16@gmail.com>
2020-07-13 18:19:21 +0100

    feat: essential redis setup

--- AdvancedNodeStarter-master/routes/blogRoutes.js
+++ AdvancedNodeStarter-master/routes/blogRoutes.js
@@ -1,31 +1,48 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    //how not to hit mdb every time
+    const redis = require("redis");
+    const redisUrl = "redis://127.0.0.1:6379";
+    const client = redis.createClient(redisUrl);
+    const util = require("util");
+    client.get = util.promisify(client.get); //instead of callback returns a promise
 
+    //do we have cache data in redis?
+    const cachedBlogs = await client.get(req.user.id);
+
+    //if yes ==> return data
+    if (cachedBlogs) {
+      console.log("serving from Redis");
+      return res.send(JSON.parse(cachedBlogs));
+    }
+    //if no ==> query and update redis
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log("serving from Mongo");
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
